2c066dbc0f8b2552b92ca329b2edc5cd17d128d3
gon <gonzalomartinezgonzalez@gmail.com>
2024-03-02 16:40:07 +0100

    javadoc: comentarios a CCuenta

--- ED-proyectoTarea04/src/cuentas/CCuenta.java
+++ ED-proyectoTarea04/src/cuentas/CCuenta.java
@@ -1,96 +1,190 @@
 package cuentas;
 
+/**
+ * Clase para la tarea. proporcionada en el enunciado
+ *
+ * @author enunciado
+ * @since 2/3/2024
+ * @version 1
+ */
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    /**
+     * Constructor por defecto
+     *
+     * @author enunciado
+     * @since 2/3/2024
+     * @version 1
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Constructor general
+     *
+     * @author enunciado
+     * @since 2/3/2024
+     * @version 1
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    /**
+     * getter de estado
+     *
+     * @author enunciado
+     * @since 2/3/2024
+     * @version 1
+     * @return saldo
+     */
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * metodo ingresar, para aumentar el saldo
+     *
+     * @author enunciado
+     * @since 2/3/2024
+     * @version 1
+     *
+     * @param cantidad
+     * @exception si se ingresa cantidad negativa
+     * @return nuevo saldo
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * metodo retirar, para reducir el saldo
+     *
+     * @author enunciado
+     * @since 2/3/2024
+     * @version 1
+     *
+     * @param cantidad
+     * @exception si se retira cantidad negativa o una cantidad mayor al saldo
+     * actual
+     * @return nuevo saldo
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 
     /**
-     * @return the nombre
+     * getter de nombre
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @return nombre
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * setter de nombre
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @param nombre
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * getter de cuenta
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @return cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * setter de cuenta
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @param cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * getter de saldo
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @return saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * setter de saldo
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @param saldo
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * getter de tipoInterés
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @return tipoInteres
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * setter de tipoIinteres
+     *
+     * @author gonzalo
+     * @since 2/3/2024
+     * @version 2
+     * @param tipoInterés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
