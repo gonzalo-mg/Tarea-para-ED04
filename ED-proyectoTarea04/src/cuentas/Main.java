c95535d41f9f2917e63e0dc7794bec1ee70760da
gon <gonzalomartinezgonzalez@gmail.com>
2024-03-02 16:21:08 +0100

    refactor: introduccion metodo operativa_cuenta

--- ED-proyectoTarea04/src/cuentas/Main.java
+++ ED-proyectoTarea04/src/cuentas/Main.java
@@ -7,9 +7,14 @@ public class Main {
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        
+        operativa_cuenta(cuenta1);
+    }
+
+    public static void operativa_cuenta(CCuenta cuenta1) {
+        double saldoActual;
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
